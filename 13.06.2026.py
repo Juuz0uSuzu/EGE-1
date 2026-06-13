90f3751cf22a810f9126453ef1df0e13f34c78d3
Juuz0uSuzu <martyshev416@gmail.com>
2026-06-13 19:32:26 +0200

    13.06.2026

--- 13.06.2026.py
+++ 13.06.2026.py
@@ -1,3 +1,64 @@
-#pierwsza
-"""dru
-ga"""
+#не должно быть символов
+#не должно начинаться с цифр
+#не должно быть пробелов
+#summ=10
+#print(summ)
+
+#int - целочисленный тип данных
+#float - дробный тип данных
+
+#name_1=10 #int
+#name_2= 10. #float
+#print(type(name_2), type(name_1))
+
+
+
+#name_1 ='hello world' #строка str
+#print(name_1, type(name_1))
+#list-список
+#name_1 =[1, 2, 3, 4 ,5]
+#tuple - кортеж
+#name_1 = (1, 2, 3 ,4, 5)
+
+#set - множество уникальных значений
+#name_1={1, 2, 3, 4, 5}
+
+#dict-словарь (множество ключ. значений)
+#name_1={"hello":10, "hello":20}
+#ключ это не все объекты
+#ключи уникальны
+
+#bool - логический тип, принимает лишь 0\1 True\False
+#name_1 = True
+
+# () -1
+# ** -степень -2
+# / * // % -3
+# +- -4
+#
+# num1=9
+# num2=2
+# print(num1*num2)
+# print(num1/num2) # всегда флоат искл. целочисленное
+# print(num1**num2)
+# print(num1//num2) # округляется в меньшую сторону
+# print(num1%num2)
+# print(num1+num2) # флоат приоритетнее инт
+# print(num1-num2)
+
+# float > int > bool
+# print(True+0)
+# string= int(input("Enter a string: "))
+# print(string+88, type(string))
+
+# print(int(float("2.1")))
+
+
+
+# num1 = int(10.2) # "10"
+# num2=float(10) # "2.1"
+# num3=list("hello world")
+# num3=set({})
+# num3=tuple({1, 2, 3 ,4})
+# print({"hello":"world"})
+num3=bool(0)
