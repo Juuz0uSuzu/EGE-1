1199e852c348785ed5d0cf2fd0cc02130a67f65c
Juuz0uSuzu <martyshev416@gmail.com>
2026-07-04 17:46:34 +0200

    04.07.2026.py

---
+++ 04.07.2026.py
@@ -0,0 +1,86 @@
+# num = abs(int(input("Введите целое число: ")))
+# k=0
+# while num > 0:
+#     k += num % 10
+#     num = num // 10
+# print(k)
+#
+# a=input()
+# counter=0
+# summ=0
+# while counter<len(a):
+#     summ += int(a[counter])
+#     counter+=1
+# print(summ)
+from itertools import count
+#
+# n=int(input())
+# cnt=0
+# while cnt < n:
+#     cnt+=1
+#     if n%cnt==0:
+#         print(cnt)
+
+import math
+# a=0
+# flag=False
+# while a<11:
+#     z=(a**2)
+#     if z>50:
+#         flag=True
+#         break
+#     print(z)
+#     a += 1
+# if not flag:
+#     print("все квадраты меньше 50")
+
+
+# n=int(input())
+# if n>=1:
+#     j=1
+#     flag=False
+#     while n-1>j:
+#         j+=1
+#         if n%j==0:
+#             print('Составное')
+#             break
+#     else:
+#         print('Простое')
+
+
+# a=input()
+# if len(a)>=5:
+#     counter=0
+#     string=''
+#     while counter < 5:
+#         print(a[counter])
+#         counter += 1
+#     print(string)
+# else:
+#     print('Строка слишком короткая')
+#
+
+# num=int(input("Enter a number: "))
+# evens,odds = 0, 0
+# while num:
+#     if num%2==0:
+#         evens+=1
+#     else:
+#         odds+=1
+#     num=num//10
+# print(evens, odds)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
